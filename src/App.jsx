0c252d8764a088442c21e02dff79792e4ba7f24d
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-03-05 14:39:22 -0500

    practicing react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <h1>Hello World!</h1>
+          <div id="hello">hello</div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
